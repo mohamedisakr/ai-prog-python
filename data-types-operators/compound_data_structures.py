ac634cac5847a6bb07f7dd318d392a40e16fd49a
mohamedisakr <mohamedisakr@gmail.com>
2022-08-01 18:05:57 +0200

    adding values to nested dictionaries quiz

---
+++ data-types-operators/compound_data_structures.py
@@ -0,0 +1,22 @@
+elements = {
+    "hydrogen": {"number": 1,
+                 "weight": 1.00794,
+                 "symbol": "H",
+                 'is_noble_gas': False},
+    "helium": {"number": 2,
+               "weight": 4.002602,
+               "symbol": "He",
+               'is_noble_gas': True}
+}
+
+print(elements['hydrogen']['is_noble_gas'])
+
+'''
+# create a new oxygen dictionary
+oxygen = {"number": 8, "weight": 15.999, "symbol": "O"}
+# assign 'oxygen' as a key to the elements dictionary
+elements["oxygen"] = oxygen
+print('elements = ', elements)
+
+print(elements["helium"]['weight'])
+'''
