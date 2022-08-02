1b03c2be24bad5e4b184e12bdcca62ec17e4b0dd
mohamedisakr <mohamedisakr@gmail.com>
2022-08-02 15:15:19 +0200

    implementing conditional statements

---
+++ L03-control-flow/if-statement.py
@@ -0,0 +1,12 @@
+phone_balance = 4
+bank_balance = 100
+
+print(f'phone balance: {phone_balance}')
+print(f'bank balance: {bank_balance}')
+
+if phone_balance < 5:
+    phone_balance += 10
+    bank_balance -= 10
+
+print(f'phone balance: {phone_balance}')
+print(f'bank balance: {bank_balance}')
